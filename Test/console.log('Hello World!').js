6222a2031a4dcc7c9b47978b2c77098b6ac6fa63
marte1337 <54943747+marte1337@users.noreply.github.com>
2022-09-26 16:10:45 +0200

    Update console.log('Hello World!').js

--- Test/console.log('Hello World!').js
+++ Test/console.log('Hello World!').js
@@ -132,6 +132,230 @@ car.lightsOn()
 */
 
 
+/*function twoNums(a,b) {
+    var c = a+b;
+    console.log(c-a+b)
+}
+twoNums(2,13)*/
+
+/*function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i, arr[i])
+    }
+}
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+listArrayItems(colors);*/
+
+/*function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == "red") {
+            console.log(i*100, "Tomato!")
+        }
+        else if (arr[i] == "yellow") {
+            console.log(i*100, "Citrus!")
+        } 
+        else if (arr[i] == "pink") {
+            console.log(i*100, "Grapefruit!")
+        }
+        else {
+            console.log(i*100, arr[i])
+        }
+    }
+}
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+listArrayItems(colors);
+*/
+
+/*function letterFinder(word, match) {
+    for (var i=0; i<word.length; i++) {
+        if (word[i]==match) {
+            console.log("Found the", match, "at", i+1)
+        } else {
+            console.log("---No match found at", i+1)
+        }
+    }
+}
+letterFinder("test", "t")*/
+
+
+/*
+//Object Literals:
+
+var table = {
+    legs: 3,
+    color: "brown",
+    priceUSD: 100,
+}
+console.log(table.color)
+
+
+var house = {
+    rooms: 3,
+    color: "brown",
+    priceUSD: 10000,
+}
+
+console.log(house);
+house.windows = 10;
+console.log(house)
+house.windows = 15;
+console.log(house)
+
+
+var house2 = {};
+house2.rooms = 4;
+house2.color = "pink";
+house2.priceUSD = 12345;
+
+//oder:
+
+var house2 = {};
+house2["rooms"] = 4;
+house2['color']= "pink";
+house2["priceUSD"] = 12345;
+console.log(house2);
+
+//interchangable:
+
+var car = {};
+car.color = "red";
+car["color"] = "green";
+car["speed"] = 200;
+car.speed = 100;
+console.log(car); // {color: "green", speed: 100}
+
+//Vorteil brackets: space characters; numbers as string data (nicht unbegingt gut); can evaluate expressions.
+
+car["number of doors"] = 4;
+console.log(car); // {color: 'green', speed: 100, number of doors: 5}
+
+car["2022"] = 1901;
+console.log(car); // {2022: 1901, color: 'green', speed: 100, number of doors: 5}
+
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed: 100,
+    altitude: 200,
+    color: "red"
+}
+for (var i = 0; i < arrOfKeys.length; i++) {
+    console.log(drone[arrOfKeys[i]])
+}*/
+
+
+/*
+//Arrays are objects:
+
+//To add new items to an array, I can use the push() method:
+var fruits = [];
+fruits.push("apple"); // ['apple']
+fruits.push('pear'); // ['apple', 'pear']
+
+//To remove the last item from an array, I can use the pop() method:  
+fruits.pop();
+console.log(fruits); // ['apple']
+
+
+
+//f.ex. a function that takes all its arguments and pushes them into an array:
+
+function arrayBuilder(one, two, three) {
+    var arr = [];
+    arr.push(one);
+    arr.push(two);
+    arr.push(three);
+    console.log(arr);
+}
+arrayBuilder('eins', 'zwei', 'drei')
+
+//Additionally, I can save this function call to a variable:
+
+var simpleArr = arrayBuilder('apple', 'pear', 'plum');
+
+console.log(simpleArr); //undefined??
+*/
+
+/*Math Objects:
+
+Number constants
+
+Here are some of the built-in number constants that exist on the Math object: 
+
+    The PI number: Math.PI 
+
+    The Euler's constant: Math.E 
+
+    The natural logarithm of 2: Math.LN2
+
+Rounding methods
+
+These include: 
+
+     Math.ceil() - rounds up to the closest integer 
+
+     Math.floor() - rounds down to the closest integer 
+
+     Math.round() - rounds up to the closest integer if the decimal is .5 or above; otherwise, rounds down to the closest integer 
+
+     Math.trunc() - trims the decimal, leaving only the integer
+
+Arithmetic and calculus methods
+
+Here is a non-conclusive list of some common arithmetic and calculus methods that exist on the Math object: 
+
+    Math.pow(2,3) - calculates the number 2 to the power of 3, the result is 8 
+
+    Math.sqrt(16) - calculates the square root of 16, the result is 4 
+
+    Math.cbrt(8) - finds the cube root of 8, the result is 2 
+
+    Math.abs(-10) - returns the absolute value, the result is 10 
+
+    Logarithmic methods: Math.log(), Math.log2(), Math.log10() 
+
+    Return the minimum and maximum values of all the inputs: Math.min(9,8,7) returns 7, Math.max(9,8,7) returns 9.
+
+    Trigonometric methods: Math.sin(), Math.cos(), Math.tan(), etc.*/
+
+//
+
+/* // Math.random(): Generates a random decimal number between 0 and 0.99
+
+    //Example: Generate number:
+Math.random();
+
+    //Save it to a variable:
+var decimal = Math.random();
+
+    //Log the value to the console
+console.log(decimal)
+
+    //...multiplied by 10:
+console.log(decimal * 10)
+
+    //...rounded:
+console.log(Math.round(decimal * 10))
+
+
+    //alt. Example Random Integer Number:
+var decimal = Math.random() * 10;
+var rounded = Math.ceil(decimal);
+console.log(rounded)
+*/
+
+
+/*
+//String cheat sheet:
+
+var greet = "Hello, ";
+var place = "World"
+
+console.log(greet.length, greet.charAt(0), greet.concat("everybody in the ", place), place.indexOf("r"), greet.lastIndexOf("l"), greet.split("l"), greet.toUpperCase()+place.toUpperCase())
+
+*/
+
+
+
 /*    //TypeOf Operator:
 //var test = typeof(345462)
 //var test = typeof(function abc () {"..."})
