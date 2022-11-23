ac1d59f0f8d11f20d51cad886ccbb1e74494903d
marte1337 <54943747+marte1337@users.noreply.github.com>
2022-11-23 16:40:36 +0100

    more on THIS

--- JavaScript/console.log('Hello World!').js
+++ JavaScript/console.log('Hello World!').js
@@ -807,65 +807,97 @@ console.log(others2); // [2, 3]
 
 
 
-//----Final assignment:--------
+// //----Final assignment:--------
 
 
-// Given variables
-const dishData = [
-    {
-        name: "Italian pasta",
-        price: 9.55
-    },
-    {
-        name: "Rice with veggies",
-        price: 8.65
-    },
-    {
-        name: "Chicken with potatoes",
-        price: 15.55
-    },
-    {
-        name: "Vegetarian Pizza",
-        price: 6.45
-    },
-]
-const tax = 1.20;
+// // Given variables
+// const dishData = [
+//     {
+//         name: "Italian pasta",
+//         price: 9.55
+//     },
+//     {
+//         name: "Rice with veggies",
+//         price: 8.65
+//     },
+//     {
+//         name: "Chicken with potatoes",
+//         price: 15.55
+//     },
+//     {
+//         name: "Vegetarian Pizza",
+//         price: 6.45
+//     },
+// ]
+// const tax = 1.20;
 
-// Implement getPrices()
-function getPrices(taxBoolean) {
-    for (var dish of dishData) {
-        let finalPrice;
-        if (taxBoolean == true) {
-            finalPrice = dish.price * tax;
-        } else if (taxBoolean == false) {
-            finalPrice = dish.price;
-        } else {
-            console.log("You need to pass a boolean to the getPrices call!");
-            return;
-        }
-        console.log("Dish: " + dish.name + " Price: $" + finalPrice);
+// // Implement getPrices()
+// function getPrices(taxBoolean) {
+//     for (var dish of dishData) {
+//         let finalPrice;
+//         if (taxBoolean == true) {
+//             finalPrice = dish.price * tax;
+//         } else if (taxBoolean == false) {
+//             finalPrice = dish.price;
+//         } else {
+//             console.log("You need to pass a boolean to the getPrices call!");
+//             return;
+//         }
+//         console.log("Dish: " + dish.name + " Price: $" + finalPrice);
+//     }
+// }
+
+// // Implement getDiscount()
+// function getDiscount(taxBoolean, guests) {
+//     getPrices(taxBoolean);
+//     if (typeof(guests) == 'number' && guests > 0 && guests < 30) {
+//         let discount = 0;
+//         if (guests < 5) {
+//             discount = 5;
+//         } else if (guests >= 5) {
+//             discount = 10;
+//         }
+//         console.log('Discount is: $' + discount);
+//     } else {
+//         console.log('The second argument must be a number between 0 and 30');
+//     }
+// }
+
+// // Call getDiscount()
+// getDiscount(true, 2);
+// getDiscount(false, 10);
+
+
+
+
+//          ----------more on THIS keyword---------
+
+
+const player = {
+    xPos: 0,
+    yPos: 0,
+    moveLeft: function(unit) {
+        this.xPos -= unit;
+    },
+    moveRight: function(unit) {
+        this.xPos += unit;
+    },
+    moveUp: function(unit) {
+        this.yPos += unit;
+    },
+    moveDown: function(unit) {
+        this.yPos -= unit;
+    },
+    getPosition() {
+        console.log(this.xPos, this.yPos)
     }
 }
 
-// Implement getDiscount()
-function getDiscount(taxBoolean, guests) {
-    getPrices(taxBoolean);
-    if (typeof(guests) == 'number' && guests > 0 && guests < 30) {
-        let discount = 0;
-        if (guests < 5) {
-            discount = 5;
-        } else if (guests >= 5) {
-            discount = 10;
-        }
-        console.log('Discount is: $' + discount);
-    } else {
-        console.log('The second argument must be a number between 0 and 30');
-    }
-}
+player.getPosition()
 
-// Call getDiscount()
-getDiscount(true, 2);
-getDiscount(false, 10);
+player.moveRight(55)
+player.moveLeft(3)
+player.moveUp(100)
+player.moveDown(8)
 
-
-
+player.getPosition()
