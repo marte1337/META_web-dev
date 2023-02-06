213ceb514e558fc3bdb2a223948f9c4709b304d6
marte1337 <54943747+marte1337@users.noreply.github.com>
2023-02-06 18:21:15 +0100

    conflict solved

--- React/firstapp/src/App.js
+++ React/firstapp/src/App.js
@@ -7,22 +7,39 @@ import Promo from './components/Promo';
 import Footer from './components/Footer';
 import { Bag, Apples, Pears } from './components/Bag'; // = Named-Export/Import
 
+//Heading-Component
 function Heading(props) {
   return (<div>
-          <h1>This is the number { Math.floor(Math.random() * 10) + 1 } heading</h1>
+          <h1>This is a random number between 1 and 10: { Math.floor(Math.random() * 10) + 1 }</h1>
           {props.newLine}
           </div>
   )
   
 }
 
+//You can pass almost any kind of expression as a prop.
+const bool = false;
+const str1 = "just";
+
+function ExprProps(props) {
+  return (
+      <div>
+          <h2>
+              The value of the toggleBoolean prop is: {props.toggleBoolean.toString()}
+          </h2>
+          <p>The value of the math prop is: <em>{props.math}</em></p>
+          <p>The value of the str prop is: <em>{props.str}</em></p>
+      </div>
+  );
+};
+
+
 
 
 function App() {
   return ( 
     <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
-      <Heading newLine={<h3>This is a new line</h3>}/> 
+      <Heading newLine={<h3>This is a new line via props</h3>}/> 
       <div>
         <Nav />
         <Intro1 />
@@ -44,6 +61,13 @@ function App() {
           <Apples color="green" number="3" />
           <Pears friend="Frank" />
         </Bag>
+      </div>
+      <div>
+        <ExprProps
+          toggleBoolean={!bool}
+          math={(10 + 20) / 3}
+          str={str1 + ' another ' + 'string'}
+        />
       </div>
     </div> 
   ); 
